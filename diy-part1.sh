eb0b8725c4008fd05583d83d80465d874d597000
heyhere <47047936+xhaomdl@users.noreply.github.com>
2021-05-22 01:40:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+sed -i '$a src-git openwrtpackages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+sed -i '$a src-git natelol https://github.com/natelol/natelol.git' feeds.conf.default
+sed -i '$a src-git bearDropper https://github.com/robzr/bearDropper' feeds.conf.default
+sed -i '$a src-git cupsdluciapp https://github.com/gdck/luci-app-cupsd' feeds.conf.default
+sed -i '$a src-git iptvhelper https://github.com/riverscn/openwrt-iptvhelper' feeds.conf.default
+sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
+sed -i '$a src-git txddns https://github.com/msylgj/OpenWrt_luci-app' feeds.conf.default
